029ffa148864921e91ced7d34c8734a7581a892b
hellobrittany <hello@brittanywetzel.com>
2017-07-11 22:14:03 -0500

    worked on vuejs and ajax calls

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -104,31 +104,41 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }.bind(this));
       },
 
-      updateLifestyleInfo: function() {
-          this.errors = [];
-          var params = {
-                        allergies: this.allergies,
-                        "reason_companion": this.reasonCompanion,
-                        "reason_kids": this.reasonKids,
-                        "reason_gift": this.reasonGift,
-                        "reason_other_pet": this.reasonOtherPet,
-                        "reason_watchdog": this.reasonWatchdog,
-                        "home_during_day": this.homeDuringDay,
-                        "when_outside": this.whenOutside,
-                        "when_inside": this.whenInside, 
-                        "hours_dog_left_alone": this.hoursDogLeftAlone, 
-                        "where_dog_sleep": this.whereDogSleep, 
-                        "outside_areas": this.outsideAreas, 
-                        "preferred_level_of_exercise": this.preferredLevelOfExercise, 
-                        "type_of_dog_food": this.typeOfDogFood,
-                        "where_dog_stay": this.whereDogStay
-                        };
-          $.post('/api/v1/applications.json', params, function(newLifestyleInfo) {
-            this.newLifestyleInfo.push(newLifestyleInfo);
-            
-          }.bind(this)).fail( function(response) {
-            this.errors = response.responseJSON.errors;
-          }.bind(this));
+      updateLifestyleInfo: function(newLifestyleInfo) {
+        this.errors = [];
+        var applicationId = document.getElementById("application-id");
+        console.log("this works");
+        var params = {
+                      allergies: this.allergies,
+                      "reason_companion": this.reasonCompanion,
+                      "reason_kids": this.reasonKids,
+                      "reason_gift": this.reasonGift,
+                      "reason_other_pet": this.reasonOtherPet,
+                      "reason_watchdog": this.reasonWatchdog,
+                      "home_during_day": this.homeDuringDay,
+                      "when_outside": this.whenOutside,
+                      "when_inside": this.whenInside, 
+                      "hours_dog_left_alone": this.hoursDogLeftAlone, 
+                      "where_dog_sleep": this.whereDogSleep, 
+                      "outside_areas": this.outsideAreas, 
+                      "preferred_level_of_exercise": this.preferredLevelOfExercise, 
+                      "type_of_dog_food": this.typeOfDogFood,
+                      "where_dog_stay": this.whereDogStay
+                      };
+        console.log("hi");
+          $.ajax({
+            url: '/api/v1/applications/' + applicationId.innerHTML + '/lifestyle_info/' + applicationId.innerHTML, 
+            type: 'PATCH', 
+            dataType: 'json', 
+            data: params, 
+
+            success: function(data) {
+                                    console.log(data);
+                                    },
+            error: function(e) {
+                                console.log(e.message);
+                                }
+          });
       },  
 
       updateOwnershipHistory: function() {
@@ -137,30 +147,121 @@ document.addEventListener("DOMContentLoaded", function(event) {
                       "number_of_current_pets": this.numberOfCurrentPets,
                       "allowed_breeding": this.allowedBreeding 
                       };
-        $.post('/api/v1/applications.json', params, function(newOwnershipHistory) {
-            this.newOwnershipHistory.push(newOwnershipHistory);
-            
-          }.bind(this)).fail( function(response) {
-            this.errors = response.responseJSON.errors;
-          }.bind(this));
-
-      },      
+        $.ajax({
+          url: '/api/v1/applications/', 
+          type: 'PATCH', 
+          dataType: 'json', 
+          data: params, 
+          success: function(data) {
+                                  console.log(data);
+                                  },
+          error: function(e) {
+                              console.log(e.message);
+                              }
+        });      
       },
 
+      updateOwnershipProfile: function() {
+        this.errors = [];
+        var params = {
+                      
+                      "regular_vet": this.regularVet,
+                      "who_grooms": this.whoGrooms, 
+                      "wears_collar": this.wearsCollar,
+                      "experience_level": this.experienceLevel, 
+                      "discipline_style": this.disciplineStyle, 
+                      "training_obedience_class": this.trainingObedienceClass, 
+                      "training_hit": this.trainingHit, 
+                      "training_verbal": this.trainingVerbal,
+                      "training_clicker": this.trainingClicker, 
+                      "training_other": this.trainingOther, 
+                      "dog_walk_style": this.dogWalkStyle, 
+                      "ready": this.ready, 
+                      "invested": this.invested, 
+                      "move_locally_plan": this.moveLocallyPlan, 
+                      "move_far_plan": this.moveFarPlan, 
+                      "move_internationally_plan": this.moveInternationallyPlan, 
+                      "gu_excessive_barking": this.guExcessiveBarking, 
+                      "gu_biting": this.guBiting, 
+                      "gu_digging": this.guDigging,
+                      "gu_moving": this.guMoving,
+                      "gu_divorce": this.guDivorce,
+                      "gu_poor_watchdog": this.guPoorWatchdog,
+                      "gu_distructive": this.guDistructive, 
+                      "gu_financial": this.guFinancial,
+                      "gu_accidents": this.guAccidents,
+                      "gu_growls": this.guGrowls,
+                      "gu_vet_bills": this.guVetBills,
+                      "gu_shedding": this.guShedding,
+                      "gu_allergies": this.guAllergies,
+                      "gu_new_partner": this.guNewPartner,
+                      "gu_aggressive": this.guAggressive,
+                      "gu_other": this.guOther
+                      };
+        $.ajax({
+          url: '/api/v1/applications/', 
+          type: 'PATCH', 
+          dataType: 'json', 
+          data: params, 
+          success: function(data) {
+                                  console.log(data);
+                                  },
+          error: function(e) {
+                              console.log(e.message);
+                              }
+        });      
+      },
 
+      updateDwellingInfo: function() {
+        this.errors = [];
+        var params = {
+                      
+                      "has_fencing": this.hasFencing,
+                      "fence_highest_height": this.fenceHighestHeight,
+                      "fence_lowest_height": this.fenceLowestHeight, 
+                      "has_gates": this.hasGates,
+                      "street_access": this.streetAccess, 
+                      "who_has_yard_access": this.whoHasYardAccess
 
+                      };
+        $.ajax({
+          url: '/api/v1/applications/', 
+          type: 'PATCH', 
+          dataType: 'json', 
+          data: params, 
+          success: function(data) {
+                                  console.log(data);
+                                  },
+          error: function(e) {
+                              console.log(e.message);
+                              }
+        });      
+      },
+    }, 
 
-
-
-
-
-
-
-
-
-
-
-
-    
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -1,2 +1,103 @@
 class Api::V1::ApplicationsController < ApplicationController
+
+
+  def show
+    @application = Application.find(params[:id])
+    @application.lifestyle_info
+    @application.dwelling_info
+    @application.ownership_history
+    @application.ownership_profile
+
+  end
+
+  def update
+    # application = Application.find(params[:id])
+    # application.assign_attributes(
+    #                                 dog_id: params[:dog_id], 
+    #                                 user_id: current_user, 
+    #                                 status: params[:status]
+    #                               )
+    lifestyle_info = LifestyleInfo.find(params[:id])
+    lifestyle_info.assign_attributes(
+                                      allergies: params[:allergies], 
+                                      reason_companion: params[:reason_companion], 
+                                      reason_kids: params[:reason_kids], 
+                                      reason_gift: params[:reason_gift], 
+                                      reason_other_pet: params[:reason_other_pet], 
+                                      reason_watchdog: params[:reason_watchdog], 
+                                      home_during_day: params[:home_during_day], 
+                                      when_inside: params[:when_inside], 
+                                      when_outside: params[:when_outside], 
+                                      hours_dog_left_alone: params[:hours_dog_left_alone], 
+                                      where_dog_sleep: params[:where_dog_sleep], 
+                                      outside_areas: params[:outside_areas], 
+                                      preferred_level_of_exercise: params[:preferred_level_of_exercise], 
+                                      type_of_dog_food: params[:type_of_dog_food]
+                                    )
+    ownership_history = OwnershipHistory.find(params[:id])
+    ownership_history.assign_attributes(
+                                        number_of_current_pets: params[:number_of_current_pets], 
+                                        allowed_breeding: params[:allowed_breeding],
+                                        )
+    ownership_history.save
+
+    ownership_profile = OwnershipProfile.find(params[:id])
+    ownership_profile.assign_attributes( 
+                                        regular_vet: params[:regular_vet],
+                                        who_grooms: params[:who_grooms], 
+                                        wears_collar: params[:wears_collar], 
+                                        experience_level: params[:experience_level], 
+                                        discipline_style: params[:experience_level], 
+                                        training_obedience_class: params[:training_obedience_class], 
+                                        training_hit: params[:training_hit], 
+                                        training_verbal: params[:training_verbal],
+                                        training_clicker: params[:training_clicker], 
+                                        training_other: params[:training_other], 
+                                        dog_walk_style: params[:dog_walk_style], 
+                                        ready: params[:ready], 
+                                        invested: params[:invested], 
+                                        move_locally_plan: params[:move_locally_plan], 
+                                        move_far_plan: params[:move_far_plan], 
+                                        move_internationally_plan: params[:move_internationally_plan], 
+                                        gu_excessive_barking: params[:gu_excessive_barking], 
+                                        gu_biting: params[:gu_biting], 
+                                        gu_digging: params[:gu_digging], 
+                                        gu_moving: params[:gu_moving], 
+                                        gu_divorce: params[:gu_divorce], 
+                                        gu_poor_watchdog: params[:gu_poor_watchdog], 
+                                        gu_distructive: params[:gu_distructive], 
+                                        gu_financial: params[:gu_financial], 
+                                        gu_accidents: params[:gu_accidents], 
+                                        gu_growls: params[:gu_growls], 
+                                        gu_vet_bills: params[:gu_vet_bills], 
+                                        gu_shedding: params[:gu_shedding], 
+                                        gu_allergies: params[:gu_allergies], 
+                                        gu_new_partner: params[:gu_new_partner], 
+                                        gu_aggressive: params[:gu_aggressive], 
+                                        gu_other: params[:gu_other]
+                                        )
+    ownership_profile.save
+
+    dwelling_info = DwellingInfo.find(params[:id])
+    dwelling_info.assign_attributes( 
+                                    has_fencing: params[:has_fencing], 
+                                    fence_highest_height: params[:fence_highest_height],
+                                    fence_lowest_height: params[:fence_lowest_height], 
+                                    has_gates: params[:has_gates], 
+                                    street_access: params[:street_access], 
+                                    who_has_yard_access: params[:who_has_yard_access]
+                                    )
+    dwelling_info.save
+
+
+
+    if application.save
+      flash[:success] = "Application updated!"
+    end
+
+  end
+
+  def destroy
+    
+  end
 end
